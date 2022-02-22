0de51e27dc79d728cb82658bf7cc95f885b56f47
MeeWeeL <MeeWeeL@yandex.ru>
2022-02-22 15:21:02 +0300

    Added more simple tests for Mapper

--- model/src/test/java/com/meeweel/delivery/model/MapperTest.kt
+++ model/src/test/java/com/meeweel/delivery/model/MapperTest.kt
@@ -25,7 +25,6 @@ class MapperTest {
     }
 
     @Test
-
     fun validator_CorrectMappingNetworkToDataModel_ReturnsTrue() {
         Assert.assertFalse(
             map.convertNetworkDTOToDataModel(
@@ -39,4 +38,102 @@ class MapperTest {
         )
     }
 
+    @Test
+    fun validator_EqualsMappingTest_ReturnsTrue() {
+        val first =
+            map.convertNetworkDTOToDataModel(
+                NetworkDTO(
+                    "drynk",
+                    "Cola",
+                    null,
+                    29
+                )
+            )
+        val second = first
+        Assert.assertEquals(first, second)
+    }
+
+    @Test
+    fun validator_NotEqualsMappingTestl_ReturnsTrue() {
+        Assert.assertNotEquals(
+            map.convertNetworkDTOToDataModel(
+                NetworkDTO(
+                    "drynk",
+                    "Cola",
+                    null,
+                    29
+                )
+            ),
+            map.convertNetworkDTOToDataModel(
+                NetworkDTO(
+                    "drynk",
+                    "Cola",
+                    null,
+                    29
+                )
+            )
+        )
+    }
+
+    @Test
+    fun validator_ArrayEqualsMappingTest_ReturnsTrue() {
+        val arr1 = arrayOf(
+            map.convertNetworkDTOToDataModel(
+                NetworkDTO(
+                    "drynk",
+                    "Cola",
+                    null,
+                    29
+                )
+            ),
+            map.convertNetworkDTOToDataModel(
+                NetworkDTO(
+                    "drynk",
+                    "Cola",
+                    null,
+                    29
+                )
+            ))
+        val arr2 = arr1
+        Assert.assertArrayEquals(arr1, arr2)
+    }
+
+    @Test
+    fun validator_NullMappingTest_ReturnsTrue() {
+        Assert.assertNull(
+            NetworkDTO(
+                "drynk",
+                "Cola",
+                null,
+                29
+            ).topping
+        )
+    }
+
+    @Test
+    fun validator_NotNullMappingTest_ReturnsTrue() {
+        Assert.assertNotNull(
+            map.convertNetworkDTOToDataModel(
+                NetworkDTO(
+                    "drynk",
+                    "Cola",
+                    null,
+                    29
+                )
+            ).picture
+        )
+    }
+
+    @Test
+    fun validator_SameMappingTest_ReturnsTrue() {
+        val thing = map.convertNetworkDTOToDataModel(
+        NetworkDTO(
+            "drynk",
+            "Cola",
+            null,
+            29
+        )
+        )
+        Assert.assertSame(thing,thing)
+    }
 }
